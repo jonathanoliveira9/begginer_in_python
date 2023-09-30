d4037a874a63b565c0a56a25825d491e4050973c
jonathanoliveirasilva9 <jonathanoliveirasilva9@gmail.com>
2023-09-30 11:18:53 -0300

    build a guess_number game

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# string concatenation (aka know to put string together)
+# suppose we want to create a string that says "subscribe to ____"
+
+# youtuber = "Kylie Ying"
+
+# # a fews to do this
+
+# print(f"subscribe_to to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+famous_person = input("Famous person:")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Say hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
